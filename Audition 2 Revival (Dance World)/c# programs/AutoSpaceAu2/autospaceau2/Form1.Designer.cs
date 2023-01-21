678268f1cdedd4fdef813d52624c43c727e2b6bc
v324s <62812546+v324s@users.noreply.github.com>
2023-01-21 17:29:19 +0400

    Add files via upload

---
+++ Audition 2 Revival (Dance World)/c# programs/AutoSpaceAu2/autospaceau2/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace autospaceau2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(39, 71);
+            this.trackBar1.Maximum = 174;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(199, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(116, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(44, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            127,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::autospaceau2.Properties.Resources.sel;
+            this.pictureBox2.Location = new System.Drawing.Point(51, 59);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1, 20);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::autospaceau2.Properties.Resources.block;
+            this.pictureBox1.Location = new System.Drawing.Point(51, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(174, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Enable";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(163, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Disable";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 214);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 111);
+            this.textBox1.TabIndex = 6;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(237, 12);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(35, 20);
+            this.numericUpDown2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 337);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.trackBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "AutospaceAU2R";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ Audition 2 Revival (Dance World)/c# programs/AutoSpaceAu2/autospaceau2/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Drawing.Printing;
+using System.Drawing.Imaging;
+
+namespace autospaceau2
+{
+    public partial class Form1 : Form
+    {
+        private static bool rabotaet = false;
+        private static int locpicx;
+        private static Random rnd = new Random();
+        private static IntPtr hwnd;
+        private static Thread potok;
+        private static int spaceX;
+        private static int spaceY;
+        private static int rezhim = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            locpicx = pictureBox2.Location.X;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = trackBar1.Value;
+            pictureBox2.Location = new Point(locpicx + trackBar1.Value, pictureBox2.Location.Y);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            trackBar1.Value = (int)numericUpDown1.Value;
+            pictureBox2.Location = new Point(locpicx + trackBar1.Value, pictureBox2.Location.Y);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rabotaet = false;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            if (potok != null)  //thrdStart - глобальная ссылка на тред
+                if (potok.IsAlive)
+                    potok.Abort();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rabotaet = true;
+            button1.Enabled = false;
+            button2.Enabled = true;
+            hwnd = FindWindow("UnrealWindow", null);
+            var rectHW = GetWindowRectEx(hwnd);
+            var rectXYXY = GetWindowRectExXY(hwnd);
+            var rectCli = GetClientRectWiHe(hwnd);
+            int windowWidth = rectHW.Width;
+            int windowHeight = rectHW.Height;
+            int windowX = rectXYXY.Left;
+            int windowY = rectXYXY.Top;
+            int window_X = rectXYXY.Right;
+            int window_Y = rectXYXY.Bottom;
+            int clientWidth = rectCli.Right;
+            int clientHeight = rectCli.Bottom;
+            int borderLRB = (windowWidth - clientWidth) / 2; //  (1296-1280)/2=8
+            int borderT = windowHeight - clientHeight - borderLRB; // 758-720=38-8 =30
+            spaceX = windowX + borderLRB + 759 - 125;
+            spaceY = windowY + borderT + 545 - 10;
+            potok = new Thread(delayscan);
+            potok.Start();
+            //func();
+        }
+        private static Bitmap screenPixel;
+        private static Bitmap obrez;
+        private static Bitmap bm;
+        private static Graphics gdest;
+        private static Graphics gsrc;
+        private void mainfunc()
+        {
+            int _x = 174;
+            int _y = 20;
+            int x = spaceX + (int)numericUpDown1.Value;
+            screenPixel=new Bitmap(_x,_y);
+            Graphics gh = Graphics.FromImage(screenPixel as Image);
+            gh.CopyFromScreen(x, spaceY, 0, 0, screenPixel.Size);
+            screenPixel.Save(String.Format(@"D:\screens\barka.png"), ImageFormat.Jpeg);
+        }
+        private void func()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                int x = spaceX + (int)numericUpDown1.Value;
+                var c = getColor(x, spaceY + 10, WindowHandle);
+                string colid = c[0] + c[1] + c[2];
+                textBox1.Text += "R=" + c[0] + " G=" + c[1] + " B=" + c[2] + '\r' + '\n';
+            }
+        }
+        private void delayscan()
+        {
+            while (rabotaet)
+            {
+                rezhim = (int)numericUpDown2.Value;
+                int x = spaceX + (int)numericUpDown1.Value;
+                var c = getColor(x, spaceY+10, WindowHandle);
+                string colid = c[0] + c[1] + c[2];
+                //Int32.Parse(c[0]) > 151 && Int32.Parse(c[1]) > 126 && Int32.Parse(c[2]) < 35
+                if (rezhim == 0)
+                {
+                    if (Int32.Parse(c[0]) > 210 && Int32.Parse(c[1]) > 130 && Int32.Parse(c[2]) < 70)
+                    {
+                        PressKey(Keys.Space, false);
+                        Thread.Sleep(rnd.Next(23, 150));
+                        PressKey(Keys.Space, true);
+                    }
+                }
+                else if (rezhim == 1)
+                {
+                    if (Int32.Parse(c[0]) > 151 && Int32.Parse(c[1]) > 126 && Int32.Parse(c[2]) < 35)
+                    {
+                        PressKey(Keys.Space, false);
+                        Thread.Sleep(rnd.Next(23, 150));
+                        PressKey(Keys.Space, true);
+                    }
+                }
+                Thread.Sleep(1);
+            }
+        }
+
+        #region DLLImport
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        public static extern int ReleaseDC(IntPtr hwnd, IntPtr hDC);
+
+        [DllImport("gdi32.dll")]
+        public static extern uint GetPixel(IntPtr hDC, int x, int y);
+        #endregion
+
+
+        public string[] getColor(int x, int y, IntPtr wndHandle)
+        {
+            byte r, g, b;
+            IntPtr hDC = GetDC(wndHandle);
+            uint pixel = GetPixel(hDC, x, y);
+            ReleaseDC(IntPtr.Zero, hDC);
+
+            r = (byte)(pixel & 0x000000FF);
+            g = (byte)((pixel & 0x0000FF00) >> 8);
+            b = (byte)((pixel & 0x00FF0000) >> 16);
+            string[] rgbpx = new string[3];
+            rgbpx[0] = r.ToString();
+            rgbpx[1] = g.ToString();
+            rgbpx[2] = b.ToString();
+            //Color color = Color.FromArgb(1, r, g, b);
+            return rgbpx;
+        }
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+        public static void PressKey(Keys key, bool up)
+        {
+            const int KEYEVENTF_EXTENDEDKEY = 0x1;
+            const int KEYEVENTF_KEYUP = 0x2;
+            if (up)
+            {
+                keybd_event((byte)key, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, (UIntPtr)0);
+            }
+            else
+            {
+                keybd_event((byte)key, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
+            }
+        }
+        public struct Rectangl
+        {
+            public readonly int Left;
+            public readonly int Top;
+            public readonly int Right;
+            public readonly int Bottom;
+            public readonly int Height;
+            public readonly int Width;
+
+            public Rectangl(int width, int height)
+                : this()
+            {
+                this.Height = height;
+                this.Width = width;
+            }
+            public Rectangl(int xx, int yy, int _xx, int _yy)
+                : this()
+            {
+                this.Left = xx;
+                this.Top = yy;
+                this.Right = _xx;
+                this.Bottom = _yy;
+            }
+        }
+        public static Rectangl GetWindowRectExXY(IntPtr hWnd)
+        {
+            if (IntPtr.Zero == hWnd) return default(Rectangl);
+
+            Rectangl lprect;
+            GetWindowRect(hWnd, out lprect);
+            return new Rectangl(lprect.Left, lprect.Top, lprect.Right, lprect.Bottom);
+        }
+        public static Rectangl GetWindowRectEx(IntPtr hWnd)
+        {
+            if (IntPtr.Zero == hWnd) return default(Rectangl);
+
+            Rectangl lprect;
+            GetWindowRect(hWnd, out lprect);
+            return new Rectangl(lprect.Right - lprect.Left, lprect.Bottom - lprect.Top);
+        }
+        public static Rectangl GetClientRectWiHe(IntPtr hWnd)
+        {
+            if (IntPtr.Zero == hWnd) return default(Rectangl);
+
+            Rectangl lprect;
+            GetClientRect(hWnd, out lprect);
+            return new Rectangl(lprect.Left, lprect.Top, lprect.Right, lprect.Bottom);
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        static extern bool GetWindowRect(IntPtr hWnd, out Rectangl lpRect);
+        [DllImport("user32.dll")]
+        static extern bool GetClientRect(IntPtr hWnd, out Rectangl lpRect);
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+        static IntPtr WindowHandle;
+
+        [DllImport("gdi32.dll", EntryPoint = "BitBlt")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool BitBlt(
+            [In()] System.IntPtr hdc, int x, int y, int cx, int cy,
+            [In()] System.IntPtr hdcSrc, int x1, int y1, uint rop);
+    }
+}
